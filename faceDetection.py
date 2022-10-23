42cfe8c9a49cc106667d2927be149f81883e84e8
HarrisonBounds <harrison.bounds777@gmail.com>
2022-10-22 20:38:08 -0500

    path and typo fix

--- faceDetection.py
+++ faceDetection.py
@@ -10,11 +10,11 @@ engine.setProperty("rate", 145) #rate of the words (WPM)
 engine.setProperty("volume", 1.0) #1 is the max volume
 
 #path to pretrained haar casacade classifiers
-path = "C:\Users\Owner\AppData\Local\Programs\Python\Python39\lib\site-packages\cv2\data"
+path = "C:\\Users\\Owner\\AppData\\Local\\Programs\\Python\\Python39\\lib\\site-packages\\cv2\\data"
 
 #Loading classifiers and assigining them to variables
-face_cascade = cv.CascadeClassifier(path + "haarcasacde_frontalface_default.xml")
-eye_cascade = cv.CascadeClassifier(path + "haarcascade_eye.xml")
+face_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_eye.xml")
 #Use addiditonal classifiers to identify profiles, smiles, glasses, etc.
 
 #changing directories to the images folder and assigning the contents to a "contents" variable
@@ -23,7 +23,7 @@ contents = sorted(os.listdir())
 
 for image in contents:
     print(f"\nFace detected...{datetime.now()}")
-    engine.say("This is a test audio for the text to speech library")
+    engine.say("test")
     engine.runAndWait() #halts the programs execution and waits for the audio to finish
     time.sleep(3)
 
@@ -32,10 +32,10 @@ for image in contents:
     #control - show the image before its face detection and wait for 2000ms
     cv.imshow(f"Face detected {image}", img_gray)
     cv.waitKey(2000)
-    cv.destroyWindow(f"Motion detected {image}")
 
     face_list = [] #empty list to hold the faces detected
-    face_list.append(face_cascade.detectMultiScale(image=img_gray, scaleFactor=1.1, minNeihbors=1))
+    face_list.append(face_cascade.detectMultiScale(image=img_gray, scaleFactor=1.1, minNeighbors=1))
+
 
 
 
